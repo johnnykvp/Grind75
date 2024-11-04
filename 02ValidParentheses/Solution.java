3ac1b7b607f7236a2e0189d3771758b5f6d61b90
Johnny Pham <59000506+johnnykvp@users.noreply.github.com>
2024-11-04 05:32:50 +0000

    Update Solution.java

--- 02ValidParentheses/Solution.java
+++ 02ValidParentheses/Solution.java
@@ -1,33 +1,32 @@
 class Solution {
-    public boolean isValid(String s) {
-        /* Clarify
-        given a string with brackets
-        return boolean if string is valid
-        valid if:
-            brackets are closed by same type
-            brackets are closed in correct order (open/close)
-        odd input is invalid
-        can be given non-bracket chars? (no, assume all brackets)
-        */
+    /* Clarify
+    given a string with brackets
+    return boolean if string is valid
+    valid if:
+        brackets are closed by same type
+        brackets are closed in correct order (open/close)
+    odd input is invalid
+    can be given non-bracket chars? (no, assume all brackets)
+    */
 
-        /* Approach
-        if statement checks string length is odd
+    /* Approach
+    if statement checks string length is odd
+        return false
+    Stack stores open brackets
+    for loop iterates through string
+        if statement checks string char == ( || { || [
+            add char to Stack
+        else if statement checks string char == ) && Stack peek == (
+            remove from Stack
+        else if statement checks string char == } && Stack peek == {
+            remove from Stack
+        else if statement checks string char == ] && Stack peek == [
+            remove from Stack
+        else
             return false
-        Stack stores open brackets
-        for loop iterates through string
-            if statement checks string char == ( || { || [
-                add char to Stack
-            else if statement checks string char == ) && Stack peek == (
-                remove from Stack
-            else if statement checks string char == } && Stack peek == {
-                remove from Stack
-            else if statement checks string char == ] && Stack peek == [
-                remove from Stack
-            else
-                return false
-        return if Stack is empty
-        */
-
+    return if Stack is empty
+    */
+    public boolean isValid(String s) {
         if (s.length() % 2 != 0) {
             return false;
         }
@@ -51,35 +50,34 @@ class Solution {
             }
         }
         return openBrackets.isEmpty();
-
-        /* Test
-        Input: s = "()[]"
-        i = 0 >> == ( >> openBrackets = "("
-        i = 1 >> == ) && peek == ( >> openBrackets = ""
-        i = 2 >> == [ >> openBrackets = "["
-        i = 3 >> == ] && peek == [ >> openBrackets = ""
-        Output: true
-
-        Input: s = "([])"
-        i = 0 >> == ( >> openBrackets = "("
-        i = 1 >> == [ >> openBrackets = "(["
-        i = 2 >> == ] >> peek == [ >> openBrackets = "("
-        i = 3 >> == ) >> peek == ( >> openBrackets = ""
-        Output: true
-
-        Input: s = "(]"
-        i = 0 >> == ( >> openBrackets = "("
-        i = 1 >> == ] >> peek != [
-        Output: false
-
-        Input: s = ")("
-        i = 0 >> == ) >> peek isEmpty
-        Output: false
-        */
-
-        /* Optimize
-        O(n) runtime, iterates through for loop based on given string length
-        O(n) space, create Stack data structure based on given string length
-        */
     }
+    /* Test
+    Input: s = "()[]"
+    i = 0 >> == ( >> openBrackets = "("
+    i = 1 >> == ) && peek == ( >> openBrackets = ""
+    i = 2 >> == [ >> openBrackets = "["
+    i = 3 >> == ] && peek == [ >> openBrackets = ""
+    Output: true
+
+    Input: s = "([])"
+    i = 0 >> == ( >> openBrackets = "("
+    i = 1 >> == [ >> openBrackets = "(["
+    i = 2 >> == ] >> peek == [ >> openBrackets = "("
+    i = 3 >> == ) >> peek == ( >> openBrackets = ""
+    Output: true
+
+    Input: s = "(]"
+    i = 0 >> == ( >> openBrackets = "("
+    i = 1 >> == ] >> peek != [
+    Output: false
+
+    Input: s = ")("
+    i = 0 >> == ) >> peek isEmpty
+    Output: false
+    */
+
+    /* Optimize
+    O(n) runtime, iterates through for loop based on given string length
+    O(n) space, create Stack data structure based on given string length
+    */
 }
