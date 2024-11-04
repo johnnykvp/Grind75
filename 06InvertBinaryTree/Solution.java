c715d725616e6de51cb1309de41f7adc1cf434c6
Johnny Pham <59000506+johnnykvp@users.noreply.github.com>
2024-11-04 05:13:34 +0000

    Update Solution.java

--- 06InvertBinaryTree/Solution.java
+++ 06InvertBinaryTree/Solution.java
@@ -14,27 +14,26 @@
  * }
  */
 class Solution {
+    /* Clarify
+    given root node of binary tree
+    invert the tree (swap left & right nodes)
+    return the root node
+    will the tree need to be sorted? (no, can be given in any order)
+    */
+
+    /* Approach
+    recursively iterate through nodes
+    if statement checks node is null
+        return null
+    store current left node (including pointers, not just value)
+    set left node equal to right node
+    set right node equal to stored left node
+    recursive call on left node
+    recursive call on right node
+    return root
+    uses DFS by recursively visiting all left nodes first, then right nodes
+    */
     public TreeNode invertTree(TreeNode root) {
-        /* Clarify
-        given root node of binary tree
-        invert the tree (swap left & right nodes)
-        return the root node
-        will the tree need to be sorted? (no, can be given in any order)
-        */
-
-        /* Approach
-        recursively iterate through nodes
-        if statement checks node is null
-            return null
-        store current left node (including pointers, not just value)
-        set left node equal to right node
-        set right node equal to stored left node
-        recursive call on left node
-        recursive call on right node
-        return root
-        uses DFS by recursively visiting all left nodes first, then right nodes
-        */
-
         if (root == null) {
             return null;
         }
@@ -44,26 +43,25 @@ class Solution {
         invertTree(root.left);
         invertTree(root.right);
         return root;
-
-        /* Test
-        Input:
-                    4            1st pass           4
-            2               7       >>      7               2
-        1       3       6       9       6       9       1       3
-        2nd pass
-                    4
-            7               2       3rd pass >> null nodes, no changes
-        9       6       3       1   
-        Output: ^
-        
-        Input: null
-        if statement catches null
-        Output: null
-        */
-
-        /* Optimize
-        O(n) runtime, based on given tree depth
-        O(n) space, created nodes based on given tree depth
-        */
     }
+    /* Test
+    Input:
+                4            1st pass           4
+        2               7       >>      7               2
+    1       3       6       9       6       9       1       3
+    2nd pass
+                4            3rd pass
+        7               2       >> null nodes, no changes
+    9       6       3       1   
+    Output: ^
+        
+    Input: null
+    if statement catches null
+    Output: null
+    */
+
+    /* Optimize
+    O(n) runtime, based on given tree depth
+    O(n) space, created nodes based on given tree depth
+    */
 }
