7e853e8e33d66bdd0250b6a039da3e8853e9e20c
Johnny Pham <59000506+johnnykvp@users.noreply.github.com>
2024-11-16 01:25:44 +0000

    Update Solution.java

--- 08BinarySearch/Solution.java
+++ 08BinarySearch/Solution.java
@@ -9,8 +9,8 @@ class Solution {
     /* Approach
     int stores left, right pointers
     while loop iterates through when left <= right
-        int stores half distance = (right - left) / 2
-        int stores mid pointer = left + half distance
+        int stores halfway distance = (right - left) / 2
+        int stores mid pointer = left + halfway distance
         if statement checks mid value = target
             return mid index
         if statement checks mid < target
@@ -23,8 +23,8 @@ class Solution {
         int leftIndex = 0;
         int rightIndex = nums.length - 1;
         while (leftIndex <= rightIndex) {
-            int half = (rightIndex - leftIndex) / 2;
-            int midIndex = leftIndex + half;
+            int halfway = (rightIndex - leftIndex) / 2;
+            int midIndex = leftIndex + halfway;
             if (nums[midIndex] == target) {
                 return midIndex;
             }
@@ -40,16 +40,16 @@ class Solution {
     /* Test
     Input: nums = [-1,0,3,5,9,12], target = 9
     rightIndex = 5 >> leftIndex 0 < rightIndex 5
-    half = 5 / 2 = 2 >> midIndex = 2 >> midVal = 3 < target 9
+    halfway = 5 / 2 = 2 >> midIndex = 2 >> midVal = 3 < target 9
     leftIndex = 3 >> leftIndex 3 < rightIndex 5
-    half = 2 / 2 = 1 >> midIndex = 4 >> midVal = 9 == target 9
+    halfway = 2 / 2 = 1 >> midIndex = 4 >> midVal = 9 == target 9
     Output: 4
 
     Input: nums = [-1,0,3,5,9,12], target = 2
     rightIndex = 5 >> leftIndex 0 < rightIndex 5
-    half = 5 / 2 = 2 >> midIndex = 2 >> midVal 3 > target 2
+    halfway = 5 / 2 = 2 >> midIndex = 2 >> midVal 3 > target 2
     rightIndex = 1 >> leftIndex 0 < rightIndex 1
-    half = 1 / 2 = 0 >> midIndex = 0 >> midVal -1 < target 2
+    halfway = 1 / 2 = 0 >> midIndex = 0 >> midVal -1 < target 2
     leftIndex = 1 >> leftIndex 1 == rightIndex 1
     Output: -1
     */
